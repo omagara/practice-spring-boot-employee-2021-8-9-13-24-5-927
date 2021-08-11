6b8641ee3b346f502d3dbd4346a0206dfcc81109
dyje <dyje@oocl.com>
2021-08-11 16:20:58 +0800

    [SpringBootEmployee][Jesse]feat: created getEmployeeByPage API

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -1,13 +1,11 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -15,7 +13,15 @@ public class EmployeesController {
 
     private final List<Employee> employees = new ArrayList<>();
     public EmployeesController(){
-    employees.add(new Employee(1, "Alice", 25, "Female", 10000));
+        employees.add(new Employee(1, "Alice", 25, "Female", 10000));
+        employees.add(new Employee(2, "Bob", 25, "Female", 10000));
+        employees.add(new Employee(3, "Catnice", 25, "Female", 10000));
+        employees.add(new Employee(4, "Doggo", 25, "Male", 10000));
+        employees.add(new Employee(5, "Edd", 25, "Male", 10000));
+        employees.add(new Employee(6, "Farla", 25, "Female", 10000));
+        employees.add(new Employee(7, "Ginger", 25, "Female", 10000));
+        employees.add(new Employee(8, "Yondu", 25, "Male", 10000));
+
     }
 
     @GetMapping
@@ -30,5 +36,15 @@ public class EmployeesController {
                 .findFirst()
                 .orElse(null);
     }
+
+    @GetMapping(params = {"page", "pageSize"})
+    public List<Employee> getEmployeeByPage (@RequestParam Integer page, @RequestParam Integer pageSize){
+        return employees.stream()
+                .skip((page -1) * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
+
+
 
 }
