2f9c28f17cb715f3e744c7ac99c9a8c95bcfc644
dyje <dyje@oocl.com>
2021-08-11 17:58:43 +0800

    [SpringBootEmployee][Jesse]refactor: extracted EmployeeService class

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.getEmployees();
+    }
+}
