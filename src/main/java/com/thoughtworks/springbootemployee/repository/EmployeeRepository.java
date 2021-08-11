877ead7852841615615a4653196e506d0fd7d008
dyje <dyje@oocl.com>
2021-08-11 17:58:20 +0800

    [SpringBootEmployee][Jesse]refactor: extracted EmployeeRepository class

---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class EmployeeRepository {
+    private List<Employee> employees = new ArrayList<>();
+    public EmployeeRepository(){
+        employees.add(new Employee(1, "Alice", 25, "Female", 10000));
+        employees.add(new Employee(2, "Bob", 25, "Female", 10000));
+        employees.add(new Employee(3, "Catnice", 25, "Female", 10000));
+        employees.add(new Employee(4, "Doggo", 25, "Male", 10000));
+        employees.add(new Employee(5, "Edd", 25, "Male", 10000));
+        employees.add(new Employee(6, "Farla", 25, "Female", 10000));
+        employees.add(new Employee(7, "Ginger", 25, "Female", 10000));
+        employees.add(new Employee(8, "Yondu", 25, "Male", 10000));
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
