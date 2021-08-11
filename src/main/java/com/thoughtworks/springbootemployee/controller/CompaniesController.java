0a1c97d7ec2c67fd0fd9d9fe0c9f5f8a7b1ec156
omagara <omagara@oocl.com>
2021-08-11 21:50:48 +0800

    [SpringBootCompany][Ramon]feat: created getAllEmployees function

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompaniesController.java
@@ -0,0 +1,38 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompaniesController {
+    List<Company> companies = new ArrayList<>();
+    List<Employee> employees = new ArrayList<>();
+    public CompaniesController() {
+        companies.add(new Company(12, "XYZ Company", 300, employees));
+        companies.add(new Company(13, "ABC Company", 300, employees));
+        companies.add(new Company(14, "EFG Company", 300, employees));
+
+        employees.add(new Employee(1, "Alice", 25, "Female", 10000));
+        employees.add(new Employee(2, "Bob", 25, "Female", 10000));
+        employees.add(new Employee(3, "Catnice", 25, "Female", 10000));
+        employees.add(new Employee(4, "Doggo", 25, "Male", 10000));
+        employees.add(new Employee(5, "Edd", 25, "Male", 10000));
+        employees.add(new Employee(6, "Farla", 25, "Female", 10000));
+        employees.add(new Employee(7, "Ginger", 25, "Female", 10000));
+        employees.add(new Employee(8, "Yondu", 25, "Male", 10000));
+    }
+
+    @GetMapping
+    public List<Company> getAllCompanies(){
+        return companies;
+    }
+
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.springbootemployee.model;
+
+import java.util.List;
+
+public class Company {
+    private Integer companyId;
+    private String companyName;
+    private Integer employeesNumber;
+    private List<Employee> employees;
+
+    public Company(Integer companyId, String companyName, Integer employeesNumber, List<Employee> employees) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+        this.employees = employees;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public Integer getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
