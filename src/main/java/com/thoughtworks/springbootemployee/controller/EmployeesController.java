2b16fe9842b85fde6e2df9986ffde782dfcc07b7
omagara <omagara@oocl.com>
2021-08-12 07:07:22 +0800

    [SpringBootEmployee][Ramon]refactor: modified the APIs of the EmployeesController class

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -24,67 +24,31 @@ public class EmployeesController {
 
     @GetMapping(path = "/{employeeID}")
     public Employee getEmployeeByID (@PathVariable Integer employeeID){
-        return employees.stream()
-                .filter(employee -> employee.getId().equals(employeeID))
-                .findFirst()
-                .orElse(null);
+        return employeeService.getEmployeesById(employeeID);
     }
 
     @GetMapping(params = {"page", "pageSize"})
     public List<Employee> getEmployeeByPage (@RequestParam Integer page, @RequestParam Integer pageSize){
-        return employees.stream()
-                .skip((page -1) * pageSize)
-                .limit(pageSize)
-                .collect(Collectors.toList());
+        return employeeService.getEmployeebyPage(page,pageSize);
     }
 
     @GetMapping(params = {"gender"} )
     public List<Employee> getEmployeeByGender (@RequestParam String gender){
-        return employees.stream()
-                .filter(employee -> employee.getGender().equalsIgnoreCase(gender))
-                .collect(Collectors.toList());
+        return employeeService.getEmployeebyGender(gender);
     }
 
     @PostMapping
     public Employee addNewEmployee (@RequestBody Employee employee){
-        employees.add(employee);
-
-        return employee;
+        return employeeService.addNewEmployee(employee);
     }
 
     @PutMapping (path = "/{employeeId}")
     public Employee updateEmployeeInfo (@PathVariable Integer employeeId, @RequestBody Employee employeeToBeUpdated){
-        return employees.stream()
-                .filter(employee -> employee.getId().equals(employeeId))
-                .findFirst()
-                .map(employee -> updateEmployee(employee, employeeToBeUpdated))
-                .get();
+        return employeeService.updateEmployeeInfo(employeeId,employeeToBeUpdated);
     }
 
     @DeleteMapping(path = "/{employeeId}")
     public void deleteEmployee(@PathVariable Integer employeeId){
-        employees.stream()
-                .filter(employee -> employee.getId().equals(employeeId))
-                .findFirst()
-                .ifPresent(employees::remove);
+        employeeService.deleteEmployee(employeeId);
     }
-
-    private Employee updateEmployee(Employee employee, Employee employeeToBeUpdated) {
-        if (employeeToBeUpdated.getName() != null){
-            employee.setName(employeeToBeUpdated.getName());
-        }
-        if (employeeToBeUpdated.getGender() != null){
-            employee.setGender(employeeToBeUpdated.getGender());
-        }
-        if (employeeToBeUpdated.getAge() != null){
-            employee.setAge(employeeToBeUpdated.getAge());
-        }
-        if (employeeToBeUpdated.getSalary() != null){
-            employee.setSalary(employeeToBeUpdated.getSalary());
-        }
-
-    return  employee;
-    }
-
-
 }
