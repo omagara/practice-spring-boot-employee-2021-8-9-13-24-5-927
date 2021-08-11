84b752a04324725628f828c839d712649eb4ba91
dyje <dyje@oocl.com>
2021-08-11 18:43:28 +0800

    [SpringBootEmployee][Jesse]refactor: refactored codes in EmployeeService.java

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 @Service
 public class EmployeeService {
-    @Autowired
     private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.getEmployees();
