e2e94ec0c0ebed6621d8b3df07d55b3d42dccfba
omagara <omagara@oocl.com>
2021-08-11 14:57:16 +0800

    [SpringBootEmployee][Ramon]feat: created getEmployeeByID API

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,14 @@ public class EmployeesController {
     @GetMapping
     public List<Employee> getEmployees(){
         return employees;
+    }
+
+    @GetMapping(path = "/{employeeID}")
+    public Employee getEmployeeByID (@PathVariable Integer employeeID){
+        return employees.stream()
+                .filter(employee -> employee.getId().equals(employeeID))
+                .findFirst()
+                .orElse(null);
     }
 
 }
