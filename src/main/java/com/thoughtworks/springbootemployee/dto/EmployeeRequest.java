9d0e959538c8f82dff77dd02502fd737f45a2dc5
omagara <omagara@oocl.com>
2021-08-13 23:41:56 +0800

    [SpringBootEmployee][Ramon]feat: added EmployeeMapper and EmployeeRequest

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeRequest.java
@@ -0,0 +1,69 @@
+package com.thoughtworks.springbootemployee.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class EmployeeRequest {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+
+
+    public EmployeeRequest() {
+
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public EmployeeRequest(Integer id, String name, Integer age, String gender, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+}
