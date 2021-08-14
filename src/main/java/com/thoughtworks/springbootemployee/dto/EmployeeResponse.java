91667163b763b668ea55d1f8faa55e1e4f5793e0
omagara <omagara@oocl.com>
2021-08-14 08:54:26 +0800

    [SpringBootEmployee][Ramon]feat: added toResponse function in EmployeeMapper and created EmployeeResponse class

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponse.java
@@ -0,0 +1,71 @@
+package com.thoughtworks.springbootemployee.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class EmployeeResponse {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+
+    public EmployeeResponse() {
+
+    }
+
+
+    public EmployeeResponse(Integer id, String name, Integer age, String gender, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+
+}
--- src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.entity.Employee;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
@@ -11,5 +12,11 @@ public class EmployeeMapper {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeRequest, employee);
         return employee;
+    }
+
+    public EmployeeResponse toResponse(Employee employee){
+        EmployeeResponse employeeResponse = new EmployeeResponse();
+        BeanUtils.copyProperties(employee, employeeResponse);
+        return employeeResponse;
     }
 }
